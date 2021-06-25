4726c43c765fba97d00d58465c1766e10c4c0158
nishu_codes <nisgautam0323@gmail.com>
2021-06-25 22:07:51 +0530

    Completed Phase 1

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
+// The above one is the url of the API
+
+var fetch = require('node-fetch');
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(response => response.json())
+    .then(result => {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+    })
